e72020ae8255705ecddb2a8098ea080ac4d1d48c
Redox <mail@redox.to>
2023-10-17 00:20:24 +0200

    Add workshop

---
+++ lua/autorun/m9k_workshop_downloads.lua
@@ -0,0 +1,6 @@
+-- Should be merged with other files in the future
+
+resource.AddWorkshop( "128089118" ) -- M9K Assault Rifles
+resource.AddWorkshop( "128091208" ) -- M9K Heavy Weapons
+resource.AddWorkshop( "128093075" ) -- M9K Small Arms Pack
+resource.AddWorkshop( "144982052" ) -- M9K Specialties
