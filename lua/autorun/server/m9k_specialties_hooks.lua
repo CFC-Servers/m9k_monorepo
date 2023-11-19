c9f765980b15eb0102d20e3599e45687a71302d4
Redox <mail@redox.to>
2023-11-19 20:12:38 +0100

    Split file to sh/sv

--- lua/autorun/server/m9k_specialties_hooks.lua
+++ lua/autorun/server/m9k_specialties_hooks.lua
@@ -1,16 +1,3 @@
-game.AddAmmoType( { name = "SatCannon", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "40mmGrenade", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "C4Explosive", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "ProxMine", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "Improvised_Explosive", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "Nuclear_Warhead", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "NerveGas", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "StickyGrenade", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "Harpoon", dmgtype = DMG_BULLET } )
-game.AddAmmoType( { name = "nitroG", dmgtype = DMG_BULLET } )
-
-game.AddParticles( "particles/nitro_main.pcf" )
-
 local function melee( victim, info )
     if not IsValid( victim ) then return end
     if not victim:IsPlayer() then return end
