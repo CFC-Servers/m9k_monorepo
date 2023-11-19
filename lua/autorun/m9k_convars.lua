76b91d412040be560f7579d0bdd7db0ca6169ae7
Redox <mail@redox.to>
2023-11-19 18:24:09 +0100

    Move convar creation to own file

--- lua/autorun/m9k_convars.lua
+++ lua/autorun/m9k_convars.lua
@@ -1,31 +1,3 @@
-/*------------------------------------------------------
-
-If you're reading this, then that mean's you've extracted this addon, probably with intentions
-of editing it for your own needs, or that you're using a legacy addon.
-
-I have no problem with that, but you must understand that I cannot offer support for legacy addons.
-If you've extracted this addon, I cannot offer any help fixing problems that come up. It's impossible
-to know what you've changed, and thus impossible to know what to fix.
-
-"But Bob!" you might say. "I only changed one thing!"
-
-Well, that's a shame. Everybody is going to say this, and I know that some of those people will be
-lying to me. The only thing I can do is to refuse support to everyone using legacy addons.
-
-So, by using a legacy addon, you accept the fact that I cannot help fix anything that might be broken.
-
-I know it's tough love, but that's the way it's got to be.
-
-------------------------------------------------------*/
-
---I'm pretty sure we don't need these anymore...
---Almost 99 percent sure that's I'm 100 percent sure...
-
--- if GetConVar("M9KDisableHolster") == nil then
-    -- CreateConVar("M9KDisableHolster", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Disable my totally worthless and broken holster system? Won't hurt my feelings any. 1 for true, 2 for false. A map change may be required.")
-    -- print("Holster Disable con var created")
--- end
-
 if GetConVar("DebugM9K") == nil then
     CreateConVar("DebugM9K", "0", { FCVAR_REPLICATED, FCVAR_ARCHIVE }, "Debugging for some m9k stuff, turning it on won't change much.")
 end
@@ -63,4 +35,29 @@ end
 if GetConVar("M9KUniqueSlots") == nil then
     CreateConVar("M9KUniqueSlots", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Give M9K Weapons unique slots? 1 for true, 2 for false. A map change may be required.")
     print("Unique Slots con var created")
+end
+
+if GetConVar("M9KExplosiveNerveGas") == nil then
+    CreateConVar("M9KExplosiveNerveGas", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Use silent explosions for nerve gas? Doesn't clip through walls, but does make other things explode.")
+    print("Explosive Nerve Gas con var created")
+end
+
+if GetConVar("M9K_Davy_Crockett_Timer") == nil then
+    CreateConVar("M9K_Davy_Crockett_Timer", "3", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Time to make Davy Crockett holder wait before firing the weapon.")
+    print("Davy Crockett timer created")
+end
+
+if GetConVar("DavyCrockettAllowed") == nil then
+    CreateConVar("DavyCrockettAllowed", "1", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Allow people to shoot the Davy Crockett?")
+    if (GetConVar("DebugM9K"):GetBool()) then print("m9k_davy_crockett blacklist convar created!") end
+end
+
+if GetConVar("DavyCrocketAdminOnly") == nil then
+    CreateConVar("DavyCrocketAdminOnly", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Make the Davy Crockett an Admin Only weapon?")
+    if (GetConVar("DebugM9K"):GetBool()) then print("DavyCrocketAdminOnly convar created!") end
+end
+
+if GetConVar("OrbitalStrikeAdminOnly") == nil then
+    CreateConVar("OrbitalStrikeAdminOnly", "0", { FCVAR_REPLICATED, FCVAR_NOTIFY, FCVAR_ARCHIVE }, "Make the Orbital Cannon an Admin Only weapon?")
+    if (GetConVar("DebugM9K"):GetBool()) then print("OrbitalStrikeAdminOnly convar created!") end
 end
