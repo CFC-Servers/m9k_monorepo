abf80ad809c8308972f881a5b344e82cdbab8c70
Redox <mail@redox.to>
2023-11-19 17:59:10 +0100

    Move sound.Add's to own file

--- lua/autorun/m9k_specialties_11_07_15.lua
+++ lua/autorun/m9k_specialties_11_07_15.lua
@@ -90,55 +90,16 @@ if GetConVar("OrbitalStrikeAdminOnly") == nil then
     if (GetConVar("DebugM9K"):GetBool()) then print("OrbitalStrikeAdminOnly convar created!") end
 end
 
-game.AddAmmoType( {
-    name = "SatCannon",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "40mmGrenade",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "C4Explosive",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "ProxMine",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "Improvised_Explosive",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "Nuclear_Warhead",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "NerveGas",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "StickyGrenade",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "Harpoon",
-    dmgtype = DMG_BULLET
-} )
-
-game.AddAmmoType( {
-    name = "nitroG",
-    dmgtype = DMG_BULLET
-} )
+game.AddAmmoType({name="SatCannon",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="40mmGrenade",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="C4Explosive",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="ProxMine",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="Improvised_Explosive",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="Nuclear_Warhead",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="NerveGas",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="StickyGrenade",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="Harpoon",dmgtype=DMG_BULLET})
+game.AddAmmoType({name="nitroG",dmgtype=DMG_BULLET})
 
 game.AddParticles("particles/nitro_main.pcf")
 
