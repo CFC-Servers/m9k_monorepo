23d64aa568f5cdc97b003ba63ae88e559732a122
Redox <mail@redox.to>
2024-04-29 04:56:50 +0200

    Fix damascus PrimaryAttack

--- lua/weapons/m9k_damascus/shared.lua
+++ lua/weapons/m9k_damascus/shared.lua
@@ -75,56 +75,61 @@ SWEP.SwordClash             = "weapons/blades/clash.mp3"
 
 function SWEP:PrimaryAttack()
     if not self:GetOwner():IsPlayer() then return end
-    local pos = self:GetOwner():GetShootPos()
-    local ang = self:GetOwner():GetAimVector()
-    local vm = self:GetOwner():GetViewModel()
+    if not self:CanPrimaryAttack() then return end
+
+    local owner = self:GetOwner()
+    local pos = owner:GetShootPos()
+    local ang = owner:GetAimVector()
+    local vm = owner:GetViewModel()
     local damagedice = math.Rand( 0.95, 1.05 )
     local pain = self.Primary.Damage * damagedice
 
-    if self:CanPrimaryAttack() and self:GetOwner():IsPlayer() then
-        self:SendWeaponAnim( ACT_VM_IDLE )
-        if not self:GetOwner():KeyDown( IN_SPEED ) and not self:GetOwner():KeyDown( IN_RELOAD ) and not self:GetOwner():KeyDown( IN_ATTACK2 ) then
-            if self.Slash == 1 then
-                --if CLIENT then return end
-                vm:SetSequence( vm:LookupSequence( "midslash1" ) )
-                self.Slash = 2
-            else
-                --if CLIENT then return end
-                vm:SetSequence( vm:LookupSequence( "midslash2" ) )
-                self.Slash = 1
-            end --if it looks stupid but works, it aint stupid!
-                self:EmitSound( self.Primary.Sound ) --slash in the wind sound here
-            if SERVER and self:GetOwner():Alive() then
-                local slash = {}
-                slash.start = pos
-                slash.endpos = pos + ( ang * 52 )
-                slash.filter = self:GetOwner()
-                slash.mins = Vector( -15, -5, 0 )
-                slash.maxs = Vector( 15, 5, 5 )
-                local slashtrace = util.TraceHull( slash )
-                if slashtrace.Hit then
-                    targ = slashtrace.Entity
-                    if targ:IsPlayer() or targ:IsNPC() then
-                        self:EmitSound( self.SwordChop )
-                        paininfo = DamageInfo()
-                        paininfo:SetDamage( pain )
-                        paininfo:SetDamageType( DMG_SLASH )
-                        paininfo:SetAttacker( self:GetOwner() )
-                        paininfo:SetInflictor( self )
-                        paininfo:SetDamageForce( slashtrace.Normal * 35000 )
-                        targ:TakeDamageInfo( paininfo )
-                    else
-                        self:EmitSound( self.KnifeShink ) --SHINK!
-                        local look = self:GetOwner():GetEyeTrace()
-                        util.Decal( "ManhackCut", look.HitPos + look.HitNormal, look.HitPos - look.HitNormal )
-                    end
-                end
-            end
+    self:SendWeaponAnim( ACT_VM_IDLE )
+    if owner:KeyDown( IN_SPEED ) then return end
+    if owner:KeyDown( IN_RELOAD ) then return end
+    if owner:KeyDown( IN_ATTACK2 ) then return end
 
-            self:GetOwner():SetAnimation( PLAYER_ATTACK1 )
-            self:SetNextPrimaryFire( CurTime() + 1 / (self.Primary.RPM / 60) )
+    if self.Slash == 1 then
+        vm:SetSequence( vm:LookupSequence( "midslash1" ) )
+        self.Slash = 2
+    else
+        vm:SetSequence( vm:LookupSequence( "midslash2" ) )
+        self.Slash = 1
+    end
+
+    self:EmitSound( self.Primary.Sound )
+
+    if owner:Alive() then
+        local slash = {}
+        slash.start = pos
+        slash.endpos = pos + ( ang * 52 )
+        slash.filter = owner
+        slash.mins = Vector( -15, -5, 0 )
+        slash.maxs = Vector( 15, 5, 5 )
+        local slashtrace = util.TraceHull( slash )
+        if slashtrace.Hit then
+            targ = slashtrace.Entity
+            if targ:IsPlayer() or targ:IsNPC() then
+                self:EmitSound( self.SwordChop )
+                if SERVER then
+                    local paininfo = DamageInfo()
+                    paininfo:SetDamage( pain )
+                    paininfo:SetDamageType( DMG_SLASH )
+                    paininfo:SetAttacker( owner )
+                    paininfo:SetInflictor( self )
+                    paininfo:SetDamageForce( slashtrace.Normal * 35000 )
+                    targ:TakeDamageInfo( paininfo )
+                end
+            else
+                self:EmitSound( self.KnifeShink )
+                local look = owner:GetEyeTrace()
+                util.Decal( "ManhackCut", look.HitPos + look.HitNormal, look.HitPos - look.HitNormal )
+            end
         end
     end
+
+    owner:SetAnimation( PLAYER_ATTACK1 )
+    self:SetNextPrimaryFire( CurTime() + 1 / ( self.Primary.RPM / 60 ) )
 end
 
 --[[ function GuardUp(victim, info)
