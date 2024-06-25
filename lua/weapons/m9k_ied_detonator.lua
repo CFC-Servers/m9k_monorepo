5644d7c18fd2b0e8f55e1b8f8effb85ff77e68dc
Redox <mail@redox.to>
2024-06-25 16:12:49 +0200

    Remove no longer needed NWent

--- lua/weapons/m9k_ied_detonator.lua
+++ lua/weapons/m9k_ied_detonator.lua
@@ -127,11 +127,9 @@ function SWEP:PrimaryAttack()
     local rocket = ents.Create( self.Primary.Round )
     if not rocket:IsValid() then return false end
 
-    rocket:SetNWEntity( "Owner", self:GetOwner() )
     rocket:SetAngles( aim:Angle() + Angle( 90, 0, 0 ) )
     rocket:SetPos( pos )
     rocket.BombOwner = self:GetOwner()
-    rocket:SetNWEntity( "Owner", self:GetOwner() )
     rocket:Spawn()
 
     local phys = rocket:GetPhysicsObject()
