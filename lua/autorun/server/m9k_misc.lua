f1e06de3099dc193ef6a98abb142e163cde183f4
Redox <git@redox.to>
2025-05-07 19:37:17 +0200

    Add setowner detour to find possible issue with m9k swep owner

--- lua/autorun/server/m9k_misc.lua
+++ lua/autorun/server/m9k_misc.lua
@@ -5,3 +5,16 @@ resource.AddWorkshop( "3114933414" ) -- Monorepo Content
 local expectedTickSpeed = 1 / 67
 local tickSpeed = engine.TickInterval()
 M9K.TickspeedMult = tickSpeed / expectedTickSpeed
+
+-- Temporary, see what's somehow causing this
+local entMeta = FindMetaTable( "Entity" )
+entMeta._SetOwner = entMeta._SetOwner or entMeta.SetOwner
+local setOwner = entMeta._SetOwner
+
+function entMeta:SetOwner( ply )
+    if self:IsWeapon() and not IsValid( ply ) then
+        ErrorNoHaltWithStack( "M9K: SetOwner called with invalid player entity. " .. tostring( self ) .. "\n" )
+    end
+
+    setOwner( self, ply )
+end
