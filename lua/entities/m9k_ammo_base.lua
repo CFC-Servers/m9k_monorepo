ee03db9b8c09a43f6ef1b8e1bd85991ec9fc544b
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-03-15 00:13:03 +0300

    Baseclass (#7)
    * Basic ammo baseclass
    
    * Special ammo baseclass
    
    * Fix possible infinity loop
    
    * Small optimization

---
+++ lua/entities/m9k_ammo_base.lua
@@ -0,0 +1,122 @@
+ENT.Base = "base_anim"
+ENT.PhysicsSounds = true
+
+if SERVER then
+    AddCSLuaFile()
+
+    local ammo_detonation = GetConVar( "M9KAmmoDetonation" )
+
+    function ENT:Initialize()
+        self:SetModel( self.Model )
+        self:PhysicsInit( SOLID_VPHYSICS )
+        self:SetUseType( SIMPLE_USE )
+        self:PhysWake()
+    end
+
+    function ENT:OnTakeDamage( dmg )
+        self:TakePhysicsDamage( dmg )
+
+        if not ammo_detonation:GetBool() or self.BlowedUp then
+            return
+        end
+
+        if self.ExplosionEffect then
+            if ( dmg:IsExplosionDamage() or math.random( 1, 15 ) == 1 ) then
+                local pos = self:GetPos()
+                self.BlowedUp = true
+
+                local bombexplosion = EffectData()
+                bombexplosion:SetOrigin( pos )
+                bombexplosion:SetRadius( 1000 )
+                bombexplosion:SetMagnitude( 1000 )
+                util.Effect( "HelicopterMegaBomb", bombexplosion )
+
+                local explosion = EffectData()
+                explosion:SetOrigin( pos )
+                explosion:SetStart( pos )
+                util.Effect( "Explosion", explosion, true, true )
+
+                local trace = util.TraceLine( {
+                    start = pos,
+                    endpos = pos + self:GetUp() * -5,
+                    filter = self
+                } )
+
+                local cinematicexplosion = EffectData()
+                cinematicexplosion:SetOrigin( pos )
+                cinematicexplosion:SetNormal( trace.Hit and trace.HitNormal or vector_up )
+                cinematicexplosion:SetEntity( self )
+                cinematicexplosion:SetScale( 4 )
+                cinematicexplosion:SetRadius( 67 )
+                cinematicexplosion:SetMagnitude( 18 )
+                util.Effect( "m9k_gdcw_cinematicboom", cinematicexplosion )
+
+                util.BlastDamage( self, dmg:GetAttacker(), pos, 1000, 800 )
+                util.ScreenShake( pos, 2000, 255, 2.5, 1250)
+
+                self:EmitSound( "C4.Explode" )
+                self:Remove()
+            end
+        else
+            if dmg:GetDamage() > 75 or math.random( 1, 5 ) == 1 then
+                local attacker = dmg:GetAttacker()
+                local pos = self:GetPos() + Vector( 0, 0, 10 )
+                self.BlowedUp = true
+
+                local effectdata = EffectData()
+                effectdata:SetOrigin( pos )
+                util.Effect( "ThumperDust", effectdata )
+                util.Effect( "Explosion", effectdata )
+
+                for i = 1, 100 do
+                    local trace = util.TraceLine( {
+                        start = pos,
+                        endpos = pos + Vector( math.Rand( -64000, 64000 ), math.Rand( -64000, 64000 ), math.Rand( 0, 64000 ) ),
+                        filter = self
+                    } )
+
+                    if IsValid( trace.Entity ) then
+                        trace.Entity:TakeDamage( 30 * math.Rand( 0.85, 1.15 ), attacker, self )
+                    end
+                end
+
+                self:Remove()
+            end
+        end
+    end
+
+    function ENT:Use( activator )
+        if activator:IsPlayer() and not self.Planted then
+            if self.AmmoGiveWeapon and not IsValid( activator:GetWeapon( self.AmmoGiveWeapon ) ) then
+                activator:Give( self.AmmoGiveWeapon )
+                activator:GiveAmmo( self.AmmoCount - 1, self.AmmoType )
+            else
+                activator:GiveAmmo( self.AmmoCount, self.AmmoType )
+            end
+
+            self:Remove()
+        end
+    end
+else
+    function ENT:Draw()
+        self:DrawModel()
+
+        if self.Text then
+            local pos = self:GetPos()
+            local pos_offset = self.PosOffset
+            pos:Add(self:GetUp() * pos_offset.x)
+            pos:Add(self:GetRight() * pos_offset.y)
+            pos:Add(self:GetForward() * pos_offset.z)
+
+            local angles = self:GetAngles()
+            local ang_offset = self.AngOffset
+            angles:RotateAroundAxis( angles:Right(), ang_offset.x )
+            angles:RotateAroundAxis( angles:Up(), ang_offset.y )
+            angles:RotateAroundAxis( angles:Forward(), ang_offset.z )
+
+            cam.Start3D2D( pos, angles, 0.07 )
+                draw.SimpleText( self.Text, self.TextFont, 31, -22, Color( 230, 45, 45 ), 1, 1 )
+            cam.End3D2D()
+        end
+    end
+end
--- lua/entities/m9k_ammo_orbitalstrike.lua
+++ lua/entities/m9k_ammo_orbitalstrike.lua
@@ -1,127 +1,18 @@
-ENT.Type           = "anim"
-ENT.Base           = "base_anim"
-ENT.PrintName      = "Orbital Strike ammo"
-ENT.Category       = "M9K Ammunition"
+AddCSLuaFile()
 
-ENT.Spawnable      = true
-ENT.AdminOnly      = false
+ENT.Base = "m9k_ammo_base"
+ENT.PrintName = "Orbital Strike ammo"
+ENT.Category = "M9K Ammunition"
+ENT.IconOverride = "entities/m9k_orbital_strike.png"
+ENT.Spawnable = true
 
 if SERVER then
-    AddCSLuaFile()
+    ENT.Model = "models/items/item_item_crate.mdl"
+    ENT.AmmoGiveWeapon = "m9k_orbital_strike"
+    ENT.AmmoType = "SatCannon"
+    ENT.AmmoCount = 1
 
-    function ENT:SpawnFunction( ply, tr )
-        if (not tr.Hit) then return end
-
-        local SpawnPos = tr.HitPos + tr.HitNormal * 16
-
-        local ent = ents.Create( "m9k_ammo_orbitalstrike" )
-
-        ent:SetPos( SpawnPos )
-        ent:Spawn()
-        ent:Activate()
-        ent.Planted = false
-
-        return ent
-    end
-
-    --[[---------------------------------------------------------
-   Name: Initialize
------------------------------------------------------------]]
-    function ENT:Initialize()
-        self.CanTool = false
-
-        local model = "models/Items/item_item_crate.mdl"
-
-        self:SetModel( model )
-
-        self:PhysicsInit( SOLID_VPHYSICS )
-        self:SetMoveType( MOVETYPE_VPHYSICS )
-        self:SetSolid( SOLID_VPHYSICS )
-        self:DrawShadow( false )
-
-        self:SetCollisionGroup( COLLISION_GROUP_NONE )
-
-        local phys = self:GetPhysicsObject()
-
-        if (phys:IsValid()) then
-            phys:Wake()
-            phys:SetMass( 40 )
-        end
-
-        self:SetUseType( SIMPLE_USE )
-    end
-
-    --[[---------------------------------------------------------
-   Name: PhysicsCollide
------------------------------------------------------------]]
-    function ENT:PhysicsCollide( data, physobj )
-        if (data.Speed > 80 and data.DeltaTime > 0.2) then
-            self:EmitSound( "Wood.ImpactHard" )
-        end
-    end
-
-    --[[---------------------------------------------------------
-   Name: OnTakeDamage
------------------------------------------------------------]]
-    function ENT:OnTakeDamage( dmginfo )
-        self:TakePhysicsDamage( dmginfo )
-        if GetConVar( "M9KAmmoDetonation" ) == nil then return end
-        if not (GetConVar( "M9KAmmoDetonation" ):GetBool()) then return end
-        local dice = math.random( 1, 15 )
-        if dice == 1 then
-        end
-    end
-
-    function ENT:OwnerCheck( attacker )
-        if IsValid( attacker ) then
-            return attacker
-        else
-            return self
-        end
-    end
-
-    function ENT:Normalizer()
-        local startpos = self:GetPos()
-
-        local downtrace = {}
-        downtrace.start = startpos
-        downtrace.endpos = startpos + self:GetUp() * -5
-        downtrace.filter = self
-        tracedown = util.TraceLine( downtrace )
-
-        if (tracedown.Hit) then
-            return (tracedown.HitNormal)
-        else
-            return (Vector( 0, 0, 1 ))
-        end
-    end
-
-    --[[---------------------------------------------------------
-   Name: Use
------------------------------------------------------------]]
-    function ENT:Use( activator, caller )
-        if (activator:IsPlayer()) and not self.Planted then
-            if activator:GetWeapon( "m9k_orbital_strike" ) == NULL then
-                activator:Give( "m9k_orbital_strike" )
-            else
-                activator:GiveAmmo( 1, "SatCannon" )
-            end
-            self:Remove()
-        end
-    end
-end
-
-if CLIENT then
-    --[[---------------------------------------------------------
-   Name: Initialize
------------------------------------------------------------]]
-    function ENT:Initialize()
-    end
-
-    --[[---------------------------------------------------------
-   Name: DrawPre
------------------------------------------------------------]]
-    function ENT:Draw()
-        self:DrawModel()
+    function ENT:OnTakeDamage( dmg )
+        self:TakePhysicsDamage( dmg )
     end
 end
